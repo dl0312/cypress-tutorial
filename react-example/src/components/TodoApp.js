a9d7d68e355db4bc3e9dee283e930915cac92ca7
Geon Lee <leegun2003@gmail.com>
2019-08-13 19:09:07 +0900

    👩‍💻3. From submisstion and XHRs

--- react-example/src/components/TodoApp.js
+++ react-example/src/components/TodoApp.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import TodoForm from './TodoForm'
 import TodoList from './TodoList'
 import Footer from './Footer'
+import { saveTodo } from '../lib/service'
 
 export default class TodoApp extends Component {
   constructor(props) {
@@ -13,9 +14,23 @@ export default class TodoApp extends Component {
       todos: []
     }
     this.handleNewTodoChange = this.handleNewTodoChange.bind(this)
+    this.handleTodoSubmit = this.handleTodoSubmit.bind(this)
   }
   handleNewTodoChange(evt) {
     this.setState({ currentTodo: evt.target.value })
+  }
+
+  handleTodoSubmit(evt) {
+    evt.preventDefault()
+    const newTodo = { name: this.state.currentTodo, isComplete: false }
+    saveTodo(newTodo)
+      .then(({ data }) =>
+        this.setState({
+          todos: this.state.todos.concat(data),
+          currentTodo: ''
+        })
+      )
+      .catch(() => this.setState({ error: true }))
   }
 
   render() {
@@ -24,8 +39,10 @@ export default class TodoApp extends Component {
         <div>
           <header className="header">
             <h1>todos</h1>
+            {this.state.error ? <span className="error">Oh no!</span> : null}
             <TodoForm
               currentTodo={this.state.currentTodo}
+              handleTodoSubmit={this.handleTodoSubmit}
               handleNewTodoChange={this.handleNewTodoChange}
             />
           </header>
