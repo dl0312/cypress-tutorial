66d09d6dcba4f546ee45f90775b65f3e5ff039e5
Geon Lee <leegun2003@gmail.com>
2019-08-13 18:19:13 +0900

    👩‍💻 2. Text inputs

--- react-example/src/components/TodoApp.js
+++ react-example/src/components/TodoApp.js
@@ -1,28 +1,33 @@
-import React, {Component} from 'react'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import TodoForm from './TodoForm'
 import TodoList from './TodoList'
 import Footer from './Footer'
-
 
 export default class TodoApp extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
+      currentTodo: '',
       todos: []
     }
+    this.handleNewTodoChange = this.handleNewTodoChange.bind(this)
+  }
+  handleNewTodoChange(evt) {
+    this.setState({ currentTodo: evt.target.value })
   }
 
-
-
-  render () {
+  render() {
     return (
       <Router>
         <div>
           <header className="header">
             <h1>todos</h1>
-            <TodoForm />
+            <TodoForm
+              currentTodo={this.state.currentTodo}
+              handleNewTodoChange={this.handleNewTodoChange}
+            />
           </header>
           <section className="main">
             <TodoList todos={this.state.todos} />
